38f9dca8d3a5259b5df0672219b186b94b64d6dd
jakeogh <github.com@v6y.net>
2022-07-17 12:15:57 -0700

    auto-commit

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import sys
+
+from setuptools import find_packages
+from setuptools import setup
+
 import fastentrypoints
-from setuptools import find_packages, setup
+
 if not sys.version_info[0] == 3:
-    sys.exit("Python 3 is required. Use: \'python3 setup.py install\'")
+    sys.exit("Python 3 is required. Use: 'python3 setup.py install'")
 
 dependencies = ["icecream", "click"]
 
@@ -17,15 +21,15 @@ config = {
     "author_email": "github.com@v6y.net",
     "description": "messagepack str input from terminal",
     "long_description": __doc__,
-    "packages": find_packages(exclude=['tests']),
-    "package_data": {"mp8": ['py.typed']},
+    "packages": find_packages(exclude=["tests"]),
+    "package_data": {"mp8": ["py.typed"]},
     "include_package_data": True,
     "zip_safe": False,
     "platforms": "any",
     "install_requires": dependencies,
     "entry_points": {
         "console_scripts": [
-            "mp8=mp8.mp8:cli",
+            "mp8=mp8.mp8:main",
         ],
     },
 }
